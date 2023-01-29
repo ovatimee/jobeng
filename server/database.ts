a89cec7ff4bf61dddeaf472e55f6040ca830d652
ovatimee <stonegee606@yahoo.com>
2023-01-29 12:58:57 +0000

    PG SSL

--- server/database.ts
+++ server/database.ts
@@ -1,15 +1,10 @@
 import { Pool } from "pg";
 
 let conn: any;
-
+const connectionString: string =
+  "postgres://jobeng_user:CvyLdhjKWgtfVARNZU9lvgXM2jzijWob@dpg-cf9vkpmn6mpv49fmm4h0-a.oregon-postgres.render.com/jobeng";
 if (!conn) {
-  conn = new Pool({
-    user:'jobeng_user',
-    host: 'dpg-cf9vkpmn6mpv49fmm4h0-a.oregon-postgres.render.com',
-    database: 'jobeng',
-    password: 'CvyLdhjKWgtfVARNZU9lvgXM2jzijWob'
-
-  });
+  conn = new Pool({ connectionString, ssl: true });
 }
 
 export { conn };

062a04878345af1fd399a1beaf3a00341160b0a8
ovatimee <stonegee606@yahoo.com>
2023-01-29 12:31:32 +0000

    PG

--- server/database.ts
+++ server/database.ts
@@ -3,7 +3,13 @@ import { Pool } from "pg";
 let conn: any;
 
 if (!conn) {
-  conn = new Pool();
+  conn = new Pool({
+    user:'jobeng_user',
+    host: 'dpg-cf9vkpmn6mpv49fmm4h0-a.oregon-postgres.render.com',
+    database: 'jobeng',
+    password: 'CvyLdhjKWgtfVARNZU9lvgXM2jzijWob'
+
+  });
 }
 
 export { conn };

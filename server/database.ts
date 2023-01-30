4a2832b0e084d3d1c9e92c23e7c5970397850425
ovatimee <stonegee606@yahoo.com>
2023-01-30 14:04:02 +0000

    PG Connection String

--- server/database.ts
+++ server/database.ts
@@ -1,8 +1,7 @@
 import { Pool } from "pg";
+const connectionString = process.env.PGSTRING;
 
 let conn: any;
-const connectionString: string =
-  "postgres://jobeng_user:CvyLdhjKWgtfVARNZU9lvgXM2jzijWob@dpg-cf9vkpmn6mpv49fmm4h0-a.oregon-postgres.render.com/jobeng";
 if (!conn) {
   conn = new Pool({ connectionString, ssl: true });
 }

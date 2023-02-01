d40cdea85f95f60bff2c46808008dd1f597c87f9
ovatimee <stonegee606@yahoo.com>
2023-02-01 14:13:17 +0000

    db

--- server/database.ts
+++ server/database.ts
@@ -1,9 +1,11 @@
 import { Pool } from "pg";
 const connectionString = process.env.PGSTRING;
 
+let production = false
+
 let conn: any;
 if (!conn) {
-  conn = new Pool({ connectionString, ssl: true });
+  conn = new Pool({ connectionString, ssl: production? true : false });
 }
 
 export { conn };

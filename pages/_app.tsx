215abeb97568a5a1443c8cfb38dfc25537f02f6d
ovatimee <stonegee606@yahoo.com>
2023-02-01 14:13:32 +0000

    Next-Auth session wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,27 @@
 import "../styles/globals.css";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
+import { SessionProvider } from "next-auth/react";
 
 import type { AppProps } from "next/app";
 
-
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }

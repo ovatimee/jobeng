4c758798d79278bfe54b06a28ca2cfc0ae1aa42a
ovatimee <stonegee606@yahoo.com>
2023-02-01 14:11:47 +0000

    MDQ's

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,21 @@ module.exports = {
       'max-sm': { 'max': '380px' },
       'md': '992px',
 
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
     },
     extend: {
       colors: {

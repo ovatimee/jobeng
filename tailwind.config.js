010a1b1dcc2929a7d28b7706d410171e6831c4a6
ovatimee <stonegee606@yahoo.com>
2023-02-01 17:57:02 +0000

    md

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      'md-md': '768px',
       // => @media (min-width: 768px) { ... }
 
       'lg': '1024px',
